baf24deb9d2dcacb212e32e55d8b2e20d6b622c2
ineedmoney03 <147415901+ineedmoney03@users.noreply.github.com>
2025-03-18 18:47:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
 def nova_funkcija():
     return "Ovo je nova funkcija."
 
+    
+def nova_metoda():
+    return "Ovo je nova metoda 2."
+
 #ovo je moj github projekt - Mate 
