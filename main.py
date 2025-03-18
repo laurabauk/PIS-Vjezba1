383b3f3a3c68a190520226b8f1fb7a2391a3bc60
laurabauk <laura7bauk@gmail.com>
2025-03-18 18:41:36 +0100

    feat: dodana nova metoda

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
 def nova_funkcija():
     return "Ovo je nova funkcija."
+
+def nova_metoda(self):
+    return "Ovo je nova metoda"
 
 #ovo je moj github projekt - Mate 
